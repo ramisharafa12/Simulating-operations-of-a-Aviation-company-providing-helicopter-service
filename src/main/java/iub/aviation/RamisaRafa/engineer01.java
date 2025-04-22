13ff92247dfe719b2466fb60fc543d2fa56ed118
Ramisha <2312264@iub.edu.bd>
2025-04-23 00:38:26 +0600

    third Commit

---
+++ src/main/java/iub/aviation/RamisaRafa/engineer01.java
@@ -0,0 +1,71 @@
+package iub.aviation.RamisaRafa;
+
+
+import java.time.LocalDate;
+
+public class engineer01 {
+    private String helicopter;
+    private String system;
+    private String problem;
+    private String replacement;
+    private String status;
+
+    public engineer01(String helicopter, String system, String problem, String replacement, String status) {
+        this.helicopter = helicopter;
+        this.system = system;
+        this.problem = problem;
+        this.replacement = replacement;
+        this.status = status;
+    }
+
+    public String getHelicopter() {
+        return helicopter;
+    }
+
+    public void setHelicopter(String helicopter) {
+        this.helicopter = helicopter;
+    }
+
+    public String getSystem() {
+        return system;
+    }
+
+    public void setSystem(String system) {
+        this.system = system;
+    }
+
+    public String getProblem() {
+        return problem;
+    }
+
+    public void setProblem(String problem) {
+        this.problem = problem;
+    }
+
+    public String getReplacement() {
+        return replacement;
+    }
+
+    public void setReplacement(String replacement) {
+        this.replacement = replacement;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "engineer01{" +
+                "helicopter='" + helicopter + '\'' +
+                ", system='" + system + '\'' +
+                ", problem='" + problem + '\'' +
+                ", replacement='" + replacement + '\'' +
+                ", status='" + status + '\'' +
+                '}';
+    }
+}
