a6dc8b00cde40b2efbbe8c4e279ceb710ab2d20d
Demonslayer <2130276@iub.edu.bd>
2025-04-23 21:12:00 +0600

    Sadaf 14 commit

---
+++ src/main/java/iub/aviation/Sadaf/RefundRequests.java
@@ -0,0 +1,42 @@
+package iub.aviation.Sadaf;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class RefundRequests {
+    @javafx.fxml.FXML
+    private TextField bookTF;
+    @javafx.fxml.FXML
+    private Label refundLB;
+    @javafx.fxml.FXML
+    private Label bookLB;
+    @javafx.fxml.FXML
+    private TextField eligibilityTF;
+    @javafx.fxml.FXML
+    private Label amountLB;
+    @javafx.fxml.FXML
+    private Label eligibilityLB;
+    @javafx.fxml.FXML
+    private TextField amountTF;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void recordRefundClickOnButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void approveRefundClickOnButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void notifyCustomerClickOnButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void searchClickOnButton(ActionEvent actionEvent) {
+    }
+}
