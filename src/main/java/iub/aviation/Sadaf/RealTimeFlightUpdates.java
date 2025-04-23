5d064c247c10abbc01f5f24482ecaaefbdf10851
Demonslayer <2130276@iub.edu.bd>
2025-04-23 21:11:03 +0600

    Sadaf 13 commit

---
+++ src/main/java/iub/aviation/Sadaf/RealTimeFlightUpdates.java
@@ -0,0 +1,28 @@
+package iub.aviation.Sadaf;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class RealTimeFlightUpdates {
+    @javafx.fxml.FXML
+    private Label flightUpdateLB;
+    @javafx.fxml.FXML
+    private TextField getLiveFlightDataTF;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void portalStatusClickOnButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void getLiveFlightDataClickOnButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void SendDelayAlertClickOnButton(ActionEvent actionEvent) {
+    }
+}
