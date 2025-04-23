07c07985f4e7c34f1eb3b28635b631abd3b6980e
Demonslayer <2130276@iub.edu.bd>
2025-04-23 20:46:24 +0600

    Sadaf second commit

---
+++ src/main/java/iub/aviation/Sadaf/IncidentReports.java
@@ -0,0 +1,51 @@
+package iub.aviation.Sadaf;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
+public class IncidentReports {
+    @javafx.fxml.FXML
+    private Label levelLB;
+    @javafx.fxml.FXML
+    private TableView incidentTV;
+    @javafx.fxml.FXML
+    private TableColumn typeTC;
+    @javafx.fxml.FXML
+    private TableColumn severityTC;
+    @javafx.fxml.FXML
+    private Label incidentReportLB;
+    @javafx.fxml.FXML
+    private ComboBox levelCb;
+    @javafx.fxml.FXML
+    private TableColumn dateTC;
+    @javafx.fxml.FXML
+    private TextField causeTF;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void stepsClickOnButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void LoadIncidentReportsClickOnButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void notifyTeamClickOnButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void stepsTF(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void assignTasksClickOnButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void saveClickOnButton(ActionEvent actionEvent) {
+    }
+}
