c2f264f4d461fa9ecfcb0a04716e45e05a73d4a4
Demonslayer <2130276@iub.edu.bd>
2025-04-23 21:12:43 +0600

    Sadaf 15 commit

---
+++ src/main/java/iub/aviation/Sadaf/RegulatoryUpdates.java
@@ -0,0 +1,34 @@
+package iub.aviation.Sadaf;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class RegulatoryUpdates {
+    @javafx.fxml.FXML
+    private Label updatesLB;
+    @javafx.fxml.FXML
+    private TextField newRulesTF;
+    @javafx.fxml.FXML
+    private TextField updatesTF;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void updatePolicyClickOnButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void notifyStaffClickOnButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void compareWithnternalPoliciesClickOnButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void getLatestRegulationsClickOnButton(ActionEvent actionEvent) {
+    }
+}
