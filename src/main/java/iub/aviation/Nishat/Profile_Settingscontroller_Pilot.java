94b7b58aaccec94cf75ab93a2698b942ff100bca
Asmaul Hosna Nishat <nishatsoyati@gmail.com>
2025-04-23 18:58:49 +0600

    new commit

--- src/main/java/iub/aviation/Nishat/Profile_Settingscontroller_Pilot.java
+++ src/main/java/iub/aviation/Nishat/Profile_Settingscontroller_Pilot.java
@@ -1,7 +1,37 @@
 package iub.aviation.Nishat;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class Profile_Settingscontroller_Pilot
 {
     @javafx.fxml.FXML
+    private TextField fxPilotContactInfo;
+    @javafx.fxml.FXML
+    private TextField PilotRenewLicenceNum;
+    @javafx.fxml.FXML
+    private TextField CirtificationPilotfx;
+    @javafx.fxml.FXML
+    private TextField fxPilotFlightTime;
+    @javafx.fxml.FXML
+    private TextArea textfxid;
+    @javafx.fxml.FXML
+    private TextField fxidPilotName;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void addpilotInformationsonAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void updatepilotInformationsonAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void deletepilotInformationsonAction(ActionEvent actionEvent) {
+    }
+}
