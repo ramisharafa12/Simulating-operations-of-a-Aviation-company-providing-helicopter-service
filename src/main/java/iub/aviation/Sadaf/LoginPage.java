f7159453241913b1b71b148d7cda085a994ece5d
Demonslayer <2130276@iub.edu.bd>
2025-04-23 21:05:49 +0600

    Sadaf 11 commit

---
+++ src/main/java/iub/aviation/Sadaf/LoginPage.java
@@ -0,0 +1,26 @@
+package iub.aviation.Sadaf;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class LoginPage {
+    javafx.fxml.FXML
+    private Label userLabel;
+    @javafx.fxml.FXML
+    private Label logLabel;
+    @javafx.fxml.FXML
+    private TextField userNameTF;
+    @javafx.fxml.FXML
+    private TextField passwordTF;
+    @javafx.fxml.FXML
+    private Label passwordLabel;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void loginClickOnButton(ActionEvent actionEvent) {
+    }
+}
