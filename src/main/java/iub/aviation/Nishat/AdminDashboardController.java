c569accbff6b7e10b95e1b871e0f22dfaeb65dae
Asmaul Hosna Nishat <nishatsoyati@gmail.com>
2025-04-23 17:23:15 +0600

    new commit

--- src/main/java/iub/aviation/Nishat/AdminDashboardController.java
+++ src/main/java/iub/aviation/Nishat/AdminDashboardController.java
@@ -1,7 +1,34 @@
 package iub.aviation.Nishat;
 
+import javafx.event.ActionEvent;
+
 public class AdminDashboardController
 {
     @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void ReportsAndAnalyticsOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void PilotManagementRadioOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void FlightSchedulingOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void adminTrainingpanelOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void PassengerBookingOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void helicopterManagementRadioOnAction(ActionEvent actionEvent) {
+    }
+}
