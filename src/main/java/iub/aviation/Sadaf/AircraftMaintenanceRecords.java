4ffbf669b82730124a3393e1ff456e3bd6f8c14a
Demonslayer <2130276@iub.edu.bd>
2025-04-23 20:55:45 +0600

    Sadaf 4 commit

---
+++ src/main/java/iub/aviation/Sadaf/AircraftMaintenanceRecords.java
@@ -0,0 +1,54 @@
+package iub.aviation.Sadaf;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class AircraftMaintenanceRecords {
+    @javafx.fxml.FXML
+    private TableColumn mainTypeTC;
+    @javafx.fxml.FXML
+    private Label mainTabLB;
+    @javafx.fxml.FXML
+    private TableColumn statusTC;
+    @javafx.fxml.FXML
+    private AnchorPane notesTC;
+    @javafx.fxml.FXML
+    private TextField secAirTF;
+    @javafx.fxml.FXML
+    private TableView maintainanceTV;
+    @javafx.fxml.FXML
+    private TableColumn techTC;
+    @javafx.fxml.FXML
+    private TableColumn dateTC;
+    @javafx.fxml.FXML
+    private Label recDetailsLB;
+    @javafx.fxml.FXML
+    private Label secAirLB;
+    @javafx.fxml.FXML
+    private TextField recordTF;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void loadClickOnButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void flagIssueClickOnButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void checkClickOnBUtton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void approveClickOnButton(ActionEvent actionEvent) {
+    }
+}
+
