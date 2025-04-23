3c90c0a1d41c4415dd06e869445630c90a910a75
Demonslayer <2130276@iub.edu.bd>
2025-04-23 21:13:28 +0600

    Sadaf 16 commit

---
+++ src/main/java/iub/aviation/Sadaf/SafetyAudits.java
@@ -0,0 +1,49 @@
+package iub.aviation.Sadaf;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
+public class SafetyAudits {
+    @javafx.fxml.FXML
+    private Label recordsLB;
+    @javafx.fxml.FXML
+    private TableColumn incidentTC;
+    @javafx.fxml.FXML
+    private ChoiceBox auditsScopeCB;
+    @javafx.fxml.FXML
+    private Label auditsScopeLB;
+    @javafx.fxml.FXML
+    private TextArea notifiTA;
+    @javafx.fxml.FXML
+    private TableView recordsTV;
+    @javafx.fxml.FXML
+    private TableColumn dateTC;
+    @javafx.fxml.FXML
+    private TableColumn actionTC;
+    @javafx.fxml.FXML
+    private Label safetyauLB;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void sendReportsClickOnButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void reviewClickOnButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void scheduleClickOnButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void noifyClickOnButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void findingClickOnButton(ActionEvent actionEvent) {
+    }
+}
