f231fc1208cf216635e0870c71ebf26093e20aac
Asmaul Hosna Nishat <nishatsoyati@gmail.com>
2025-04-23 22:38:46 +0600

    new commit

--- src/main/java/iub/aviation/SceneSwitcher.java
+++ src/main/java/iub/aviation/SceneSwitcher.java
@@ -1,6 +1,4 @@
 package iub.aviation;
-
-
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -11,7 +9,7 @@ import java.io.IOException;
 public class SceneSwitcher {
 
     public static void switchTo(Stage stage, String fxmlFileName) throws IOException {
-        Parent root = FXMLLoader.load(SceneSwitcher.class.getResource(fxmlFileName + ".fxml"));
+        Parent root = FXMLLoader.load(HelloApplication.class.getResource(fxmlFileName + ".fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
