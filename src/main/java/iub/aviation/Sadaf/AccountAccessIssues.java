54c053125f8ad06f90152b8b4ba42c5bd68fa399
Demonslayer <2130276@iub.edu.bd>
2025-04-23 20:51:05 +0600

    Sadaf 3 commit

--- src/main/java/iub/aviation/Sadaf/AccountAccessIssues.java
+++ src/main/java/iub/aviation/Sadaf/AccountAccessIssues.java
@@ -6,7 +6,6 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 public class AccountAccessIssues {
-
     @javafx.fxml.FXML
     private TextArea issueTA;
     @javafx.fxml.FXML
