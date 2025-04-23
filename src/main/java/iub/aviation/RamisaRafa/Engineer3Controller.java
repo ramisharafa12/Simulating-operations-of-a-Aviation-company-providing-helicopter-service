309cb66ce2acc398a9a1e03a7953e410818427d4
Ramisha <2312264@iub.edu.bd>
2025-04-23 19:48:43 +0600

    engineer controller class updated

--- src/main/java/iub/aviation/RamisaRafa/Engineer3Controller.java
+++ src/main/java/iub/aviation/RamisaRafa/Engineer3Controller.java
@@ -1,4 +1,34 @@
 package iub.aviation.RamisaRafa;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class Engineer3Controller {
+    @javafx.fxml.FXML
+    private TextField assignengineerTF;
+    @javafx.fxml.FXML
+    private TextField FaultdetailsTF;
+    @javafx.fxml.FXML
+    private TableColumn HelicopterTC;
+    @javafx.fxml.FXML
+    private TextField RepairnotesTF;
+    @javafx.fxml.FXML
+    private TableColumn AssignengineerTC;
+    @javafx.fxml.FXML
+    private ComboBox helicopterCB;
+    @javafx.fxml.FXML
+    private TableView tableview;
+    @javafx.fxml.FXML
+    private TableColumn FaultdetailsTC;
+
+    @javafx.fxml.FXML
+    public void updatestatusOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void addOA(ActionEvent actionEvent) {
+    }
 }
