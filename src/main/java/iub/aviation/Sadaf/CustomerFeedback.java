7ef6470d8f8410f8629a558a4c71127a07154edd
Demonslayer <2130276@iub.edu.bd>
2025-04-23 21:00:10 +0600

    Sadaf 7 commit

---
+++ src/main/java/iub/aviation/Sadaf/CustomerFeedback.java
@@ -0,0 +1,36 @@
+package iub.aviation.Sadaf;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class CustomerFeedback {
+    @javafx.fxml.FXML
+    private Label customerFeedLB;
+    @javafx.fxml.FXML
+    private TextField feedbackTF;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void saveRecordsClickOnButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void loadFeedbackClickOnButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void sortClickOnButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void frequentIssueClickOnButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void summaryClickOnButton(ActionEvent actionEvent) {
+    }
+}
