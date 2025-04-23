561fbfbf3b9b4dbcfa29098ebe81a7c5eb81d3d4
Demonslayer <2130276@iub.edu.bd>
2025-04-23 21:10:24 +0600

    Sadaf 12 commit

---
+++ src/main/java/iub/aviation/Sadaf/PilotCertifications.java
@@ -0,0 +1,50 @@
+package iub.aviation.Sadaf;
+
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class PilotCertifications {
+    @javafx.fxml.FXML
+    private TextField pilotNameTF;
+    @javafx.fxml.FXML
+    private Label medicalLB;
+    @javafx.fxml.FXML
+    private TextField renewTF;
+    @javafx.fxml.FXML
+    private Label pilotLB;
+    @javafx.fxml.FXML
+    private TextField medicalTF;
+    @javafx.fxml.FXML
+    private TextField licenceTF;
+    @javafx.fxml.FXML
+    private ListView alertLV;
+    @javafx.fxml.FXML
+    private Label pilotNAmeLB;
+    @javafx.fxml.FXML
+    private Label renewLB;
+    @javafx.fxml.FXML
+    private Label alertLB;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void validateClickOnButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void searchClickOnButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void licenceLB(Event event) {
+    }
+
+    @javafx.fxml.FXML
+    public void generateClickOnButton(ActionEvent actionEvent) {
+    }
+}
