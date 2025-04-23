45bd63ddc5262328f5c5fa1db147a7b6b9a7f8d1
Demonslayer <2130276@iub.edu.bd>
2025-04-23 21:14:16 +0600

    Sadaf 17 commit

---
+++ src/main/java/iub/aviation/Sadaf/TrainingSessions.java
@@ -0,0 +1,51 @@
+package iub.aviation.Sadaf;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
+public class TrainingSessions {
+    @javafx.fxml.FXML
+    private DatePicker trainDateDP;
+    @javafx.fxml.FXML
+    private Label trainigTitleLB;
+    @javafx.fxml.FXML
+    private Label materialLB;
+    @javafx.fxml.FXML
+    private TextField trainingTitleTF;
+    @javafx.fxml.FXML
+    private TableColumn nameTC;
+    @javafx.fxml.FXML
+    private TableColumn scoreTC;
+    @javafx.fxml.FXML
+    private Label trainLB;
+    @javafx.fxml.FXML
+    private TableView trainingTV;
+    @javafx.fxml.FXML
+    private Label trainDateLB;
+    @javafx.fxml.FXML
+    private TableColumn completionTC;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void completeClickOnButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void notifyStaffClickOnButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void uploadVideoClickOnButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void uploadDocsClickOnButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void issueClickOnButton(ActionEvent actionEvent) {
+    }
+}
