c5200e070e1d47a08e0343d9eff2b4b078084425
Demonslayer <2130276@iub.edu.bd>
2025-04-23 21:09:29 +0600

    Sadaf 11 commit

---
+++ src/main/java/iub/aviation/Sadaf/LostBaggageClaims.java
@@ -0,0 +1,38 @@
+package iub.aviation.Sadaf;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class LostBaggageClaims {
+    @javafx.fxml.FXML
+    private TextField passengerNameTF;
+    @javafx.fxml.FXML
+    private Label passengerNameLB;
+    @javafx.fxml.FXML
+    private Label buggageClaimLB;
+    @javafx.fxml.FXML
+    private Label flightNoLB;
+    @javafx.fxml.FXML
+    private TextField buggageIdTF;
+    @javafx.fxml.FXML
+    private Label buggageIdLB;
+    @javafx.fxml.FXML
+    private TextField flightNoTF;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void notifyClickOnButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void checkTrackingLogsClickOnButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void closeClaimClickOnButton(ActionEvent actionEvent) {
+    }
+}
