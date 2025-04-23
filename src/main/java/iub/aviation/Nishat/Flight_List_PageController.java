6aa940b4f8bff2573f202392d71383372e5c2ae6
Asmaul Hosna Nishat <nishatsoyati@gmail.com>
2025-04-23 18:20:55 +0600

    new commit

--- src/main/java/iub/aviation/Nishat/Flight_List_PageController.java
+++
@@ -1,7 +0,0 @@
-package iub.aviation.Nishat;
-
-public class Flight_List_PageController
-{
-    @javafx.fxml.FXML
-    public void initialize() {
-    }}
--- src/main/java/iub/aviation/Nishat/Flight_ScheduleController.java
+++ src/main/java/iub/aviation/Nishat/Flight_ScheduleController.java
@@ -1,7 +1,26 @@
 package iub.aviation.Nishat;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+
 public class Flight_ScheduleController
 {
     @javafx.fxml.FXML
+    private ComboBox flightFromfxid;
+    @javafx.fxml.FXML
+    private ComboBox flightToFxif;
+    @javafx.fxml.FXML
+    private DatePicker datepicker;
+    @javafx.fxml.FXML
+    private TextArea flightdetails;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void searchForFlightSchrdulrOnAction(ActionEvent actionEvent) {
+    }
+}
