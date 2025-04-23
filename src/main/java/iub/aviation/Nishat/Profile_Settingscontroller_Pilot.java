5ef2a9001895aa59d0290bc2a0bea6b8c75e0d2a
Asmaul Hosna Nishat <nishatsoyati@gmail.com>
2025-04-23 16:35:46 +0600

    new commit

---
+++ src/main/java/iub/aviation/Nishat/Profile_Settingscontroller_Pilot.java
@@ -0,0 +1,7 @@
+package iub.aviation.Nishat;
+
+public class Profile_Settingscontroller_Pilot
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
---
+++ src/main/java/iub/aviation/Nishat/Training_CertificationController_Pilot.java
@@ -0,0 +1,7 @@
+package iub.aviation.Nishat;
+
+public class Training_CertificationController_Pilot
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
