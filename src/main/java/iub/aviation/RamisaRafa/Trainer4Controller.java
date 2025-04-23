9000f7fa359ea91db658894806e15c20225ee833
Ramisha <2312264@iub.edu.bd>
2025-04-23 20:17:49 +0600

    trainer 1,2,3 controller class updated

--- src/main/java/iub/aviation/RamisaRafa/Trainer4Controller.java
+++ src/main/java/iub/aviation/RamisaRafa/Trainer4Controller.java
@@ -1,4 +1,31 @@
 package iub.aviation.RamisaRafa;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class Trainer4Controller {
+    @javafx.fxml.FXML
+    private TableColumn SimulatornameTC;
+    @javafx.fxml.FXML
+    private TableColumn PerformanceReportTC;
+    @javafx.fxml.FXML
+    private TableColumn equipmentnameTC;
+    @javafx.fxml.FXML
+    private TextField equipmentnameTF;
+    @javafx.fxml.FXML
+    private TextField PerformanceReportTF;
+    @javafx.fxml.FXML
+    private TableView Tableview;
+    @javafx.fxml.FXML
+    private TextField SimulatornameTF;
+
+    @javafx.fxml.FXML
+    public void exportOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void requestmaintainenceOA(ActionEvent actionEvent) {
+    }
 }
